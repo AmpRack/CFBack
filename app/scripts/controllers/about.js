f2790b31afe732ee27b96b35494bb6042a151de1
Patrick San Juan <pat.sanjuan@gmail.com>
2015-04-25 17:28:30 -0700

    modified portfolio background color

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name codifyForumApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the codifyForumApp
+ */
+angular.module('codifyForumApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
